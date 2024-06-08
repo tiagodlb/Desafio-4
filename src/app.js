ee5e5dde54f1b1d2d299799e88c53cae48812a7f
tiago <tigolimab223@gmail.com>
2024-06-08 14:42:25 -0300

    feature: database connection and prisma ORM

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import cors from "cors";
+import express, { json } from "express";
+import "express-async-errors";
+// import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+// import router from "./routers/index";
+
+const app = express();
+app.use(json());
+app.use(cors());
+// app.use(router); 
+// app.use(errorHandlerMiddleware);
+export default app;
