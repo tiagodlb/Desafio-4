13d86284f7379c0341e3dbf6f7fb318abd76d195
tiago <tigolimab223@gmail.com>
2024-06-18 11:24:34 -0300

    feature/feature errormiddlewarer working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import cors from "cors";
 import express, { json } from "express";
 import router from "./routers/index.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 
 const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 export default app;
