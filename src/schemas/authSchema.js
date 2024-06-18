a5285cb540fc024b079f89f2a050cf9c222c0666
tiago <tigolimab223@gmail.com>
2024-06-18 11:24:03 -0300

    feature/schema update

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -1,12 +1,27 @@
 import Joi from "joi";
 
+/*
+  Database Schema
+
+  email    String   
+  name     String
+  nickname String 
+  password String
+  birthday DateTime
+
+  */
+
 /* Cadastro */
 export const signUpSchema = Joi.object({
     email: Joi.string().required(),
-    name: Joi.string().required()
+    name: Joi.string().required(),
+    nickname: Joi.string().required(),
+    password: Joi.string().required(),
+    birthday: Joi.string().required()
 })
 
 /* Login */
 export const signInSchema = Joi.object({
     email: Joi.string().required(),
+    password: Joi.string().required()
 });
