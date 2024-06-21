2308aeb9b2f1634288865062a80a91d50b49a845
Tiago <tigolimab223@gmail.com>
2024-06-21 14:30:07 -0300

    Update userService.js
    Updating the functions so I don't push all user information

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,13 +5,17 @@ async function findUserById(id) {
     const user = await userRepository.findById(id);
     if (!user) throw notFoundError("Usuário não encontrado");
 
-    return user;
+    return { "id": user.id, "email": user.email, "nickname": user.nickname, "birthday": user.birthday };
 }
 
 async function findUsers() {
     const users = await userRepository.findUsers();
-    return users;
+    let mappedUser = users.filter((user) => {
+        return user.id && user.email && user.nickname && user.birthday
+    })
+    return mappedUser;
 }
+
 
 const userService = {
     findUserById,
