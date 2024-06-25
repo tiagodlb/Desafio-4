85273d5865cd917621109bff4dbba3ba7bc1099f
ShadeHacker <154840423+MarcoskDev@users.noreply.github.com>
2024-06-25 02:04:50 -0300

    requisição e envio do form login envioForm.js

---
+++ src/envioForm.js
@@ -0,0 +1,29 @@
+document
+   .getElementById("formLogin")
+   .addEventListener("submit", async function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      console.log("Body:", JSON.stringify({ email, password }));
+
+      try {
+        const response = await fetch("https://desafio-4.onrender.com/signin", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+         });
+         
+
+         if (!response.ok) {
+            throw new Error("Erro na requisição");
+         }
+
+         const data = await response.json();
+         console.log(data.token);
+      } catch (error) {
+         console.error("Erro:", error);
+      }
+   });
