626dae99a314e18801a4db8f94ed0d1885918ad6
tiago <tigolimab223@gmail.com>
2024-06-25 22:39:19 -0300

    fix

--- src/envioForm.js
+++ src/envioForm.js
@@ -1,6 +1,6 @@
 document
    .getElementById("formLogin")
-   .addEventListener("submit", async function (event) {
+   .addEventListener("click", async function (event) {
       event.preventDefault();
 
       const email = document.getElementById("email").value;
@@ -22,6 +22,44 @@ document
 
          const data = await response.json();
          console.log(data.token);
+      } catch (error) {
+         console.error("Erro:", error);
+      }
+   });
+
+document
+   .getElementById("formLogin")
+   .addEventListener("submit", async function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const nickname = document.getElementById("nome").value;
+      const birthday = document.getElementById("data_nascimento").value;
+
+      const data2 = {
+         email: email,
+         nickname: nickname,
+         password: password,
+         birthday: birthday
+      }
+
+      try {
+         const response = await fetch("https://desafio-4.onrender.com/signup", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data2),
+         });
+
+
+         if (!response.ok) {
+            throw new Error("Erro na requisição");
+         }
+
+         const data3 = await response.json();
+         console.log(data3.token);
       } catch (error) {
          console.error("Erro:", error);
       }
